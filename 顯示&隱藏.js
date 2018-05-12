998e19871c0b22988bf93b86d5840b54cd482652
superdragons <31409390+superdragons@users.noreply.github.com>
2018-05-13 00:21:07 +0800

    Create 顯示&隱藏.js

---
+++ 顯示&隱藏.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+  var odiv=document.getElementById("tabs");
+  var obt=document.getElementById("bt");
+  obt.onclick=function(){
+    if(odiv.style.display=="none"){
+      odiv.style.display="block";
+      this.value="全部隱藏";
+    }else{
+      odiv.style.display="none";
+      this.value="顯示";
+    }
+  }
+}
